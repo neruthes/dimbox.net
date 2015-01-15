738983d708e35dbae4ba21779a81deea225f927d
Joy Neop <joyneop@gmail.com>
2015-01-15 06:58:08 +0000

    dynamic current page indicator test 0

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+// Indicate current location on nav bar
+(function () {
+	var currentPage = window.location.pathname;
+	var nav = document.getElementsByClassName('nav-links').getElementsByTagName('a');
+	for (var i = 0; i < nav.length; i++) {
+		if (nav[i] == currentPage) {
+			nav[i].classList.add('page-now-on');
+			break;
+		};
+	};
+})();
+
 // Show commit stamp
 (function () {
 	var commitStamp = new XMLHttpRequest();
 	commitStamp.open('GET', 'commit-stamp.txt', false);
 	commitStamp.send();
-	document.getElementById('commit-stamp').innerHTML = '<code>Commit stamp: ' + commitStamp.responseText + '</code>';
+	document.getElementById('commit-stamp').innerHTML = 'Commit stamp: ' + commitStamp.responseText;
 })();
